5a1a7c26b214c9274569de7a352fd890d512a274
jungmin <jungmini0601@gmail.com>
2025-08-04 16:16:53 +0900

    [Feature] - 로그인 API

--- core-storage/database/src/main/java/blog/jungmini/me/database/repository/UserRepository.java
+++ core-storage/database/src/main/java/blog/jungmini/me/database/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package blog.jungmini.me.database.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import blog.jungmini.me.database.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByEmail(String email);
 }
