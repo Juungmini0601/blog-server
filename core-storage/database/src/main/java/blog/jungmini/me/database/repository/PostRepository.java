e52dbe512ff6e3b00bd64c630c708bf10fd60af0
jungmin <jungmini0601@gmail.com>
2025-08-05 18:20:28 +0900

    [Feature] 게시글 수정 API

--- core-storage/database/src/main/java/blog/jungmini/me/database/repository/PostRepository.java
+++ core-storage/database/src/main/java/blog/jungmini/me/database/repository/PostRepository.java
@@ -6,10 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import blog.jungmini.me.common.error.CustomException;
+import blog.jungmini.me.common.error.ErrorType;
 import blog.jungmini.me.database.entity.PostEntity;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    default PostEntity findByIdOrElseThrow(Long postId) {
+        return findById(postId)
+                .orElseThrow(() -> new CustomException(
+                        ErrorType.VALIDATION_ERROR, String.format("게시글을 찾을 수 없습니다 ID: [%d]", postId)));
+    }
 
     @Modifying
     @Transactional
